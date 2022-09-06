021c23c368bba068a6c8cc00a58e798348e74b67
princejoogie <princejoogie@gmail.com>
2022-09-06 09:47:00 +0800

    chore: update markdown components

--- src/components/markdown/index.tsx
+++ src/components/markdown/index.tsx
@@ -10,9 +10,37 @@ export const Markdown = ({ content }: MarkdownProps) => {
     <article className="prose-prose-pre:p-0 prose-lg prose-invert prose-pre:m-0 prose-pre:my-4 prose-pre:rounded-md prose-pre:p-0">
       <ReactMarkdown
         components={{
+          h1: ({ ...rest }) => (
+            <h1
+              {...rest}
+              className="my-4 border-b-2 border-gray-800 pb-3 text-3xl font-semibold lg:text-4xl"
+            />
+          ),
+          h2: ({ ...rest }) => (
+            <h2
+              {...rest}
+              className="my-4 border-b border-gray-800 pb-2 text-2xl font-semibold lg:text-3xl"
+            />
+          ),
+          h3: ({ ...rest }) => (
+            <h3 {...rest} className="my-4 text-xl font-semibold lg:text-2xl" />
+          ),
+          h4: ({ ...rest }) => (
+            <h4 {...rest} className="my-4 text-lg font-semibold lg:text-xl" />
+          ),
+          h5: ({ ...rest }) => (
+            <h5 {...rest} className="my-4 text-lg font-semibold lg:text-xl" />
+          ),
+          h6: ({ ...rest }) => (
+            <h6 {...rest} className="my-4 text-lg font-semibold lg:text-xl" />
+          ),
+          p: ({ className, ...rest }) => (
+            <p {...rest} className="my-3 text-justify text-gray-300" />
+          ),
+          ul: ({ ordered, ...rest }) => <ul {...rest} className="list-disc" />,
           img: ({ src, alt, ...rest }) => (
             // eslint-disable-next-line @next/next/no-img-element
-            <img {...rest} src={src} alt={alt} className="!my-2 inline" />
+            <img {...rest} src={src} alt={alt} className="my-0 inline" />
           ),
           code: ({ node, inline, className, children, ...props }) => {
             const language = /language-(\w+)/.exec(className ?? "")?.[1];
@@ -29,7 +57,7 @@ export const Markdown = ({ content }: MarkdownProps) => {
               return (
                 <CodeBlock
                   {...props}
-                  content={String(children).replace(/\n$/, "")}
+                  content={children.toString().replace(/\n$/, "")}
                   language={language}
                 />
               );
