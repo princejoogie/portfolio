6926bc6bab42b5187a9b20e441de8cc0dbe5929b
princejoogie <princejoogie@gmail.com>
2022-09-06 10:12:36 +0800

    perf: reduce bundle size by 50%
    - import only needed languages for highlighting

--- src/components/markdown/code-block.tsx
+++ src/components/markdown/code-block.tsx
@@ -1,5 +1,18 @@
-import { PrismAsync as Highlighter } from "react-syntax-highlighter";
+import Highlighter from "react-syntax-highlighter/dist/cjs/prism-light";
+import javascript from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import typescript from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
+import jsx from "react-syntax-highlighter/dist/cjs/languages/prism/jsx";
+import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
+import bash from "react-syntax-highlighter/dist/cjs/languages/prism/bash";
+import json from "react-syntax-highlighter/dist/cjs/languages/prism/json";
 import { highlighterStyle } from "./style";
+
+Highlighter.registerLanguage("javascript", javascript);
+Highlighter.registerLanguage("typescript", typescript);
+Highlighter.registerLanguage("jsx", jsx);
+Highlighter.registerLanguage("tsx", tsx);
+Highlighter.registerLanguage("bash", bash);
+Highlighter.registerLanguage("json", json);
 
 interface CodeBlockProps {
   language?: string;
