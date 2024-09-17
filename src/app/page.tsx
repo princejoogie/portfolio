251815a2cf2e9cfc2e2a84f2d0a993710f212b1a
princejoogie <princejoogie@gmail.com>
2024-09-17 19:36:56 +0800

    chore: add setup section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import { Contact } from "@/components/home";
 import { About } from "@/components/home/about";
 import { Blogs } from "@/components/home/blogs";
+import { Contact } from "@/components/home";
+import { Setup } from "@/components/home/setup";
 import { Tabs } from "@/components/tabs";
 import { getAllBlogsMeta } from "@/lib/mdx";
 import { Suspense } from "react";
@@ -16,7 +17,7 @@ export default async function Home() {
         tabContent={{
           About: <About blogs={blogs.slice(0, 2)} />,
           Blogs: <Blogs blogs={blogs} />,
-          Setup: <div>Setup.</div>,
+          Setup: <Setup />,
           Contact: <Contact />,
           Newsletter: <div>Working on it, stay tuned!</div>,
         }}
